bbe0e0edd0168fa0d69dda50a55601e42ad7fcb7
brunosilv <bluisilva@hotmail.com>
2020-09-21 11:11:26 +0100

    chore/trying

--- release.sh
+++ release.sh
@@ -12,7 +12,7 @@ releaseBranch=release/$versionLabel
 git checkout -b release/$versionLabel
 
 # create release PR
-gh pr create -b master -t "release/$versionLabel" --head release/$versionLabel
+gh pr create -b master -t "release/$versionLabel" -f -h develop
 
 # merge release branch with the new version number into master
 git checkout $masterBranch
